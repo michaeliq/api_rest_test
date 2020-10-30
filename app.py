91e0bdf4e70b68f3170d1708c2096d6596bb27ed
michaeliq <michaelesacol@gmail.com>
2020-10-29 22:45:26 -0500

    se adapto el formato JSON a el estandar actual, se adapto cada metodo de la API para que funcione correctamente con JSON, en jsonData.py se cambio json.loads y json.dumps a json.load y json.dump respectivamente.

--- app.py
+++ app.py
@@ -1,69 +1,91 @@
 from flask import Flask, request, jsonify
 from jsonData import read_file, write_file
+from os import path
 
 app = Flask(__name__)
 
-items = []
+if path.exists('items.json') != True:
+    with open('items.json','w') as file_istance:
+        items = {}
+        write_file('items.json',items)
+
+def key_(object_json):
+    return 'item'+str(len(object_json))
 
 @app.route('/')
 def test_w():
-    test_ = [{'marco argente':'apellido','carmen magnolia':'nombre'}]
-    json_ = write_file('items.json',test_)
+    key_ = key_(items)
+    items[key_] = []
+    items[key_].append({'marco argente':'apellido','carmen magnolia':'nombre'})
+    json_ = write_file('items.json',items)
     return json_
 
 @app.route('/write_and_read/')
 def test_rw():
-    json_ = read_file('items.json')
-    test_ = {'apellido':'cortez','nombre':'josué'}
-    json_.append(test_)
-    return write_file('items.json',json_)
+    items = read_file('items.json')
+    key = key_(items)
+    items[key] = []
+    items[key].append({'apellido':'cortez','nombre':'josué'})
+    write_file('items.json',items)
+    return items
 
 @app.route('/items/',methods=['GET'])
 def getItems():
-
+    items = read_file('items.json')
     return jsonify({'message':'items in stock',
                     'items': items})
 
-@app.route('/item/<int:name_item>/',methods=['GET'])
+@app.route('/items/<string:name_item>/',methods=['GET'])
 def getItem(name_item):
-    
-    item_founded = [item for item in items if item['name'] == name_item]
+    items = read_file('items.json')
+    item_founded = items.keys()
+    item_founded = filter(lambda x: items.get(x)[0]['name']==name_item, item_founded)
+    item_founded = list(item_founded)
     if len(item_founded) > 0:
         return jsonify({'message':'item founded!',
-                        'item': item_founded})
+                        'item': items[item_founded[0]]})
     return jsonify({'message':'item not founded'})
 
 @app.route('/items/',methods=['POST'])
 def addItem():
-    
+    items = read_file('items.json')
+    key = key_(items)
+    items[key] = []
+
     item = {
         'name':request.json['name'],
         'description':request.json['description'],
         'group':request.json['group']}
-    items.append(item)
-    print(items)
-     
+    items[key].append(item)
+    write_file('items.json',items)
     return jsonify({'message':'Item added'})
 
 @app.route('/items/<string:name_item>/',methods=['PUT'])
 def editItem(name_item):
-    
-    if item['name'] in items:
-        item['name']=request.json['name']
-        item['description']=request.json['description']
-        item['group']=request.json['group']
-        
-        return ({'message':'item edited!',
-                 'items': items})
+    items = read_file('items.json')
+
+    for item_key, item_value in items.items():
+        if item_value[0]['name'] == name_item:
+            item_value[0]['name']=request.json['name']
+            item_value[0]['description']=request.json['description']
+            item_value[0]['group']=request.json['group']
+
+            items[item_key] = item_value
+            write_file('items.json',items)
+            return jsonify({'message':'item edited!',
+                            'items': items})
     return jsonify({'message':'item not founded'})
 
 @app.route('/items/<string:name_item>/',methods=['DELETE'])
 def deleteItem(name_item):
-    
-    item_founded = [item for item in items if item['name'] == name_item]
+    items = read_file('items.json')
+    item_keys = items.keys()
+
+    item_founded = filter(lambda x: items.get(x)[0]['name'] == name_item, item_keys)
+    item_founded = list(item_founded)
     if len(item_founded) > 0:
-        items.remove(item_founded)
-        
+        items.pop(item_founded[0], 'object not found')
+        write_file('items.json',items)
         return jsonify({'message':'item deleted',
                         'items': items})
     return jsonify({'message':'item not founded'})
